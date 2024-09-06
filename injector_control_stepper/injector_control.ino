6eb0c0588e81828384ec161357fe165127f30bd6
downingr1 <downingr1@udayton.edu>
2024-09-06 12:02:45 -0400

    consolidate inject/extract fncs

--- injector_control_stepper/injector_control.ino
+++ injector_control_stepper/injector_control.ino
@@ -35,9 +35,17 @@ void stop() {
 
 
 
-void inject() {
+void start_injector(bool inject) {
   float flow, lin_speed, vol;
-  Serial.println("**Injection Mode** \nInput Flow Rate (nL/min):"); 
+
+  if (inject) {
+    Serial.println("**Injection Mode**"); 
+  }
+  else {
+    Serial.println("**Extraction Mode**");
+  }
+  
+  Serial.println("Input Flow Rate (nL/min):"); 
   while (true) {
     if (Serial.available()) {                            
       flow = Serial.parseFloat(); //returns a zero if it times out or no valid ints are available
@@ -71,16 +79,21 @@ void inject() {
   
   steps = 1 / (HAM_7000_5_DIV * lead / vol / stepsprev);
   
-  Serial.println("\nReady. Hit Physical Button to Inject."); 
+  Serial.println("\nReady. Hit Physical Button to begin."); 
   
   while (true) {
     if (digitalRead(13)==HIGH) {                            
       break; // Hangs until button is pressed
     }
   }
-
-  Serial.println("injecting...");
-  injector.move(-steps);
+  if (inject) {
+    Serial.println("Injecting...");
+    injector.move(-steps);
+  }
+  else {
+    Serial.println("Extracting...");
+    injector.move(steps);
+  }
   injector.setSpeed(steppsec);
   
   while(true) {
@@ -96,83 +109,21 @@ void inject() {
   steps = 0;
 }
 
-
-
-void extract() {
-  float flow, lin_speed, vol;
-
-  Serial.println("**Extraction Mode** \nInput Flow Rate (nL/min):"); 
-  while (true) {
-    if (Serial.available()) {                            
-      flow = Serial.parseFloat(); //returns a zero if it times out or no valid ints are available
-      break;
-    }
-  }
-
-  lin_speed = flow / HAM_7000_5_DIV;   
-
-  float rpm = lin_speed / lead; 
-  float steppsec = rpm / 60.0 * 200.0;
-
-  
-  Serial.println("\nInput Volume (nL):"); 
-  while (true) {
-    if (Serial.available()) {                            
-      vol = Serial.parseFloat(); //returns a zero if it times out or no valid ints are available
-      break;
-    }
-  }
-  
-  //Debug Info
-  Serial.print("flow rate: ");
-  Serial.println(flow);
-  Serial.print("lin_speed: ");
-  Serial.println(lin_speed);
-  Serial.print("rpm: ");
-  Serial.println(rpm);
-  Serial.print("vol: ");
-  Serial.println(vol); 
-  
-  steps = 1 / (HAM_7000_5_DIV * lead / vol / stepsprev);
-  
-    Serial.println("\nReady. Hit Physical Button to Extract."); 
-  
-  while (true) {
-    if (digitalRead(13)==HIGH) {                            
-      break; // Hang until the button is pressed
-    }
-  }
-
-  Serial.println("Extracting...");
-  injector.move(steps);
-  injector.setSpeed(steppsec);
-  
-  while(true) {
-    injector.runSpeedToPosition();
-
-    if (injector.currentPosition()==injector.targetPosition()) {
-        injector.setCurrentPosition(0);
-        restart = true;
-        break;
-    }
-  }
-  
-  steps = 0;
-}
-
-
-
-void setup() {
-  Serial.begin(115200);      //Set Baud Rate (115200 default rate in VS Code Serial Monitor)
-  Serial.println("Run keyboard control");
-  Serial.println("This Injector is controlled through serial communication. This has been tested using VS Code's arduino extension, though Arduino IDE's serial monitor or any other serial interface should work.");
-  Serial.println("In VS Code, click into the message box below and type the desired value. Then, hit enter to send the message to the injector.");
+void printInstructions() {
   Serial.println("Input the number associated with a mode to begin: ");
   Serial.println("`1` - Extraction Mode");
   Serial.println("`2` - Injection Mode");
   Serial.println("`0` - Stop");
   Serial.println();
   Serial.println("Press the reset button on the device to exit any mode and see this message again.");
+}
+
+void setup() {
+  Serial.begin(115200);      //Set Baud Rate (115200 default rate in VS Code Serial Monitor)
+  Serial.println("Run keyboard control");
+  Serial.println("This Injector is controlled through serial communication. This has been tested using VS Code's arduino extension, though Arduino IDE's serial monitor or any other serial interface should work.");
+  Serial.println("In VS Code, click into the message box below and type the desired value. Then, hit enter to send the message to the injector.");
+  printInstructions();
   injector.setCurrentPosition(0);
   injector.setMaxSpeed(1000);
 }
@@ -181,15 +132,8 @@ void setup() {
 
 void loop() {
   if(restart){
-    //Serial.println("This Injector is controlled through serial communication. This has been tested using VS Code's arduino extension, though Arduino IDE's serial monitor or any other serial interface should work.");
-    //Serial.println("In VS Code, click into the message box below and type the desired value. Then, hit enter to send the message to the injecctor.");
     Serial.println("Finished\n");
-    Serial.println("Input the number associated with a mode to begin: ");
-    Serial.println("`1` - Extraction Mode");
-    Serial.println("`2` - Injection Mode");
-    Serial.println("`0` - Stop");
-    Serial.println();
-    Serial.println("Press the reset button on the device to exit any mode and see this message again.");
+    printInstructions();
     restart = false;
   }
   if(Serial.available()){
@@ -200,10 +144,10 @@ void loop() {
       switch(val)
       {
       case '1': // Mode to Fill the syringe
-        extract ();   
+        start_injector (false);   
         break;
       case '2': // Mode for injection
-        inject ();
+        start_injector (true);
         break;
 
       case '0':
